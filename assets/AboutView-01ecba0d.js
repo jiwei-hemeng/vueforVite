5d5c59cd8188b2b67a78b6fd66c9c512a15b2286
jijiwei <jijiwei@lilianinfo.com>
2023-05-18 18:16:40 +0800

    deploy

--- assets/AboutView-01ecba0d.js
+++
@@ -1,1 +0,0 @@
-import{u as o}from"./index-f9f928bf.js";const n={class:"about"},a={__name:"AboutView",setup(u){const e=o();function t(){e.setToken(Date.now())}return(s,c)=>(Vue.openBlock(),Vue.createElementBlock("div",n,[Vue.createElementVNode("div",null,"token:"+Vue.toDisplayString(Vue.unref(e).token),1),Vue.createElementVNode("button",{onClick:t},"设置token")]))}};export{a as default};
---
+++ assets/NotFound404-dc40c164.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-7e45735a.js";const c={};function n(o,t){return Vue.openBlock(),Vue.createElementBlock("div",null,"404")}const _=e(c,[["render",n]]);export{_ as default};
