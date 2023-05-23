0fca80d8b3da75e1405068335323f2f210b5a964
jijiwei <jijiwei@lilianinfo.com>
2023-05-23 17:10:07 +0800

    通过scp 命令上传到服务器

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,6 @@ git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:jiwei-hemeng/vueforVite.git master:gh-pages
-
+# 通过scp 命令上传到服务器
+# scp -r -C -P 8022 /d/web/viteForVue/dist u0_a238@192.168.1.110:~/dist
 cd -
