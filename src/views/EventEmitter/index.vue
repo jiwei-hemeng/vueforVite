d9f199d912c073a05317df66d31ed86d7782ad59
jiwei-hemeng <jiwei7065@sina.com>
2023-11-10 13:40:36 +0800

    测试indexDB 游标分页

--- src/views/EventEmitter/index.vue
+++ src/views/EventEmitter/index.vue
@@ -14,7 +14,9 @@ import PageTop from "./pageTop.vue";
 import PageBotton from "./pagebotton.vue";
 import IndexDB from "@/utils/indexDB.js";
 const allData = await IndexDB.readAll();
-const timeList = await IndexDB.getDataByIndex('moduleType', 'timeStarp');
-console.log("all-data", allData)
-console.log("timeList", timeList)
+const timeList = await IndexDB.getDataByIndex("moduleType", "timeStarp");
+const cursorList = await IndexDB.cursorGetDataByIndexAndPage("moduleType", "routers", 3, 2);
+console.log("all-data", allData);
+console.log("timeList", timeList);
+console.log("cursorList", cursorList);
 </script>
