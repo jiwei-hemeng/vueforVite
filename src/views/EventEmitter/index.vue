63fb044ef6e6c61095c75b5034811f7a154ca8a7
jijiwei <jijiwei@lilianinfo.com>
2023-11-10 10:52:47 +0800

    使用indexDB

--- src/views/EventEmitter/index.vue
+++ src/views/EventEmitter/index.vue
@@ -10,6 +10,12 @@ export default {
 };
 </script>
 <script setup>
-import PageTop from "./pageTop.vue"
-import PageBotton from "./pagebotton.vue"
+import PageTop from "./pageTop.vue";
+import PageBotton from "./pagebotton.vue";
+import IndexDB from "@/utils/indexDB.js";
+const indexDB = await IndexDB.getIndexDB();
+const allData = await indexDB.readAll();
+const timeList = await indexDB.getDataByIndex('moduleType', 'timeStarp');
+console.log("all-data", allData)
+console.log("timeList", timeList)
 </script>
