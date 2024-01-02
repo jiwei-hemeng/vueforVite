cbb329c0701d01b76f6334052c62746330ac0bdb
jiwei-hemeng <jiwei7065@sina.com>
2024-01-02 15:10:05 +0800

    去掉-调查年份和当前年份不是同一年不能保存

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import router from "@/router";
 import IndexDB from "@/utils/indexDB.js";
 import "@/assets/main.css";
 IndexDB.getIndexDB().then(() => {
-  console.log("2224446666666")
+  console.log("222444666666644")
   debugger;
   const app = createApp(App);
   app.use(pinia);
